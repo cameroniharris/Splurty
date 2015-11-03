cf645138150f4b7473d5338438b0a3ad4296d2cd
Cameron Harris <cameroniharris1@gmail.com>
2015-11-03 15:34:52 +0000

    added show page

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "quote_show_page" do
+    quote = Quote.create(:author => 'Ken Mazaika', :saying => 'You only yolo once.')
+    get :show, :id => quote.id 
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+  	get :show, :id => 'omg'
+  	assert_response :not_found
+  end
+
 end
